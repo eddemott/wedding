8aff57dce54abf4839796fe2f8e4ac80b4663e13
eddemott <edward.de.mott@gmail.com>
2019-05-03 16:20:36 +0100

    new npm and grid on wedding page

--- src/components/AdditionalInfo.js
+++ src/components/AdditionalInfo.js
@@ -8,28 +8,28 @@ const AdditionalInfo = () => (
     <div className="row">
       <div className="col-lg-6 col-xs-12 first-lg">
         <div className="box left">
-        <div className="content">
-          <h2>Sunday</h2>
-          <p>
-            Many of you have travelled far and wide to celebrate with us, and we
-            would like to spend as much time with you as we can! Becca's Mum and
-            Stepdad will be opening their doors from 12pm until 3pm for anyone
-            who is still around, to enjoy a ploughman's lunch, and nurse our
-            hangovers together. Please let us know in the RSVP section if you
-            think you’ll be popping by.
+          <div className="content">
+            <h2>Sunday</h2>
+            <p>
+              Many of you have travelled far and wide to celebrate with us, and we
+              would like to spend as much time with you as we can! Becca's Mum and
+              Stepdad will be opening their doors from 12pm until 3pm for anyone
+              who is still around, to enjoy a ploughman's lunch, and nurse our
+              hangovers together. Please let us know in the RSVP section if you
+              think you’ll be popping by.
             <br />
-            <span>Address : </span>
-            <Link
-              href="https://goo.gl/maps/V5V6NP8BBMSkTnm86"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Gate Cottage, Stammergate Lane, Linton, LS22 4JB
+              <span>Address : </span>
+              <Link
+                href="https://goo.gl/maps/V5V6NP8BBMSkTnm86"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Gate Cottage, Stammergate Lane, Linton, LS22 4JB
             </Link>
-            <br />
-            From the main road in Linton, follow the white railings down a steep
-            little hill next to The Windmill Pub, and you'll find Gate Cottage
-            just down there on the left.
+              <br />
+              From the main road in Linton, follow the white railings down a steep
+              little hill next to The Windmill Pub, and you'll find Gate Cottage
+              just down there on the left.
           </p>
           </div>
         </div>
@@ -38,87 +38,84 @@ const AdditionalInfo = () => (
         <div className="box"><div className="image image1" alt="Socks" /></div>
       </div>
     </div>
-
     <div className="row">
-          <div className="col-lg-5 col-xs-12 first-xs">
+      <div className="col-lg-5 col-xs-12 first-xs">
         <div className="box"><div className="image image2" alt="Socks" /></div>
       </div>
       <div className="col-lg-7 col-xs-12 first-lg">
         <div className="box right">
-        <div className="content">
-        <h2>Harrogate</h2>
-          <p>
-            There are a few direct trains from London to Harrogate each day,
-            however trains run frequently to York or Leeds, where you can
-            interchange easily.
+          <div className="content">
+            <h2>Harrogate</h2>
+            <p>
+              There are a few direct trains from London to Harrogate each day,
+              however trains run frequently to York or Leeds, where you can
+              interchange easily.
           </p>
-          <p>
-            There are hotels and B&Bs to suit every budget in and around
-            Harrogate and Knaresborough. The wedding party will be staying at
-            the White Hart Hotel, which is a short walk from the ceremony venue.
-            We have secured a number of rooms at a fixed rate of £109 per
-            night. To book one of these rooms, please email Rachel at
+            <p>
+              There are hotels and B&Bs to suit every budget in and around
+              Harrogate and Knaresborough. The wedding party will be staying at
+              the White Hart Hotel, which is a short walk from the ceremony venue.
+              We have secured a number of rooms at a fixed rate of £109 per
+              night. To book one of these rooms, please email Rachel at
             <Link
-              href="mailto:office@whiteharthotelharrogate.com?Subject=Room Booking - GA00265"
-              target="_top"
-            >
-              &nbsp;office@whiteharthotelharrogate.com&nbsp;
+                href="mailto:office@whiteharthotelharrogate.com?Subject=Room Booking - GA00265"
+                target="_top"
+              >
+                &nbsp;office@whiteharthotelharrogate.com&nbsp;
             </Link>
-            quoting the code GA00265.
+              quoting the code GA00265.
           </p>
           </div>
         </div>
       </div>
-</div>
-
+    </div>
     <div className="row">
       <div className="col-lg-7 col-xs-12 first-lg">
         <div className="box left">
-        <div className="content">
-        <h2>Gift List</h2>
-          <p>
-            Your presence at our wedding is gift enough, but if you do wish to
-            buy us something to mark this occasion then we have a selected a few
-            items for our newly renovated home, and some vouchers to help us
-            along the way with planning our dream honeymoon.
+          <div className="content">
+            <h2>Gift List</h2>
+            <p>
+              Your presence at our wedding is gift enough, but if you do wish to
+              buy us something to mark this occasion then we have a selected a few
+              items for our newly renovated home, and some vouchers to help us
+              along the way with planning our dream honeymoon.
           </p>
-          <p>
-            <Link
-              href="https://prezola.com/wishlists/10205205/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Prezola Gift List
+            <p>
+              <Link
+                href="https://prezola.com/wishlists/10205205/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Prezola Gift List
             </Link>
-          </p>
+            </p>
           </div>
-          </div>
+        </div>
       </div>
       <div className="col-lg-5 col-xs-12 first-xs">
         <div className="box"><div className="image image3" alt="Sun Pavillion Image" /></div>
       </div>
     </div>
-
     <div className="row">
-    <div className="col-lg-5 col-xs-12 first-xs">
+      <div className="col-lg-5 col-xs-12 first-xs">
         <div className="box"><div className="image image4" alt="Bus" /></div>
       </div>
       <div className="col-lg-7 col-xs-12 first-lg">
         <div className="box right">
-        <div className="content">
-        <h2>Contact</h2>
-          <p>
-            If you have any questions, or need any further information then
+          <div className="content">
+            <h2>Contact</h2>
+            <p>
+              If you have any questions, or need any further information then
             please don't hesitate to contact Becca on <span> </span>
-            <Link href="tel:07552783044">07552783044</Link>, or at{" "}
-            &nbsp;
+              <Link href="tel:07552783044">07552783044</Link>, or at{" "}
+              &nbsp;
             <Link
-              href="mailto:rebeccaculleton@hotmail.co.uk?Subject=Wedding Help"
-              target="_top"
-            >
-              rebeccaculleton@hotmail.co.uk.
+                href="mailto:rebeccaculleton@hotmail.co.uk?Subject=Wedding Help"
+                target="_top"
+              >
+                rebeccaculleton@hotmail.co.uk.
             </Link>
-            &nbsp; Ed has no idea what's going on...
+              &nbsp; Ed has no idea what's going on...
           </p>
           </div>
         </div>
@@ -131,32 +128,26 @@ const Info = styled.div`
   .top {
   padding-top: 5em;
   }
-
   .content {
     padding: 5rem;
   }
-
   h2 {
     font-size: 2.25rem;
     font-weight: 400;
     letter-spacing: -0.05em;
     margin: 0 0 1rem 0;
   }
-
   p, a {
     font-size: 1.25rem;
     font-weight: 300;
     line-height: 1.7;
   }
-
   .left {
   text-align: right;
   }
-
   .right {
   text-align: left;
   }
-
   .image {
     background-position: center;
     background-repeat: no-repeat;
@@ -176,7 +167,6 @@ const Info = styled.div`
   .image4 {
     background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787849/bestival.jpg");
   }
-
   @media only screen and (max-width: 1180px) {
     .content {
       padding: 2em;
@@ -190,6 +180,6 @@ const Info = styled.div`
     .image {
     height: 30vh;
   }
+  }
 `;
-
 export default AdditionalInfo;
--- src/components/Wedding.js
+++ src/components/Wedding.js
@@ -2,87 +2,72 @@ import React from "react";
 import styled from "styled-components";
 
 import Link from "./Link";
-import Line from "./line";
+import Line from "./line"
 
 const WeddingPage = () => (
-  <div>
-      <Banner>
-      <div className="content">
-        <h1>On the day</h1>
-        <Line />
-      </div>
-      <div className="imageBanner" />
-    </Banner>
-
   <Wedding>
-<div className="row top">
-      <div className="col-lg-5 col-xs-12 first-xs">
-          <div className="box"><div className="image image1" alt="Socks" /></div>
+        <div className="row">
+      <div className="col-lg-6 col-xs-12 first-lg">
+        <div className="box center">
+        <h2 className="title">On The Day</h2>
+          <Line />
+        </div>
       </div>
-        <div className="col-lg-7 col-xs-12 first-lg">
-          <div className="box right">
-            <div className="content">
-                <h2>The Ceremony</h2>
-                    <p>
-                        The Ceremony will take place at the Sun Pavilion, which is within
-                        easy walking distance of the centre of Harrogate. The ceremony will
-                        begin promptly at 1:30 pm. If you are coming from further afield
-                        please give yourself plenty of time to get there as Harrogate
-                        traffic can be heavy on a Saturday. The grooms party will be
-                        enjoying a drink in the Fat Badger Public House beforehand if you
-                        wish to arrive earlier.
-                        <br />
-                        After the ceremony, transport will be provided to the reception
-                        venue, where there is limited space for parking.
-                    </p>
-                    <div className="links">
-                <Link href="http://thefatbadgerharrogate.com" rel="noopener noreferrer" target="_blank">The Fat Badger</Link>
-                <Link href="http://www.sunpavilion.co.uk/" rel="noopener noreferrer" target="_blank" className="Pavillion"> The Sun Pavillion</Link>
-            </div>
+      <div className="col-lg-6 col-xs-12 first-xs">
+        <div className="box">
+          <div className="image first image4" alt="Socks" />
+        </div>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-lg-5 col-xs-12 first-xs">
+        <div className="box">
+          <div className="image image1" alt="Socks" />
+        </div>
+      </div>
+      <div className="col-lg-7 col-xs-12 first-lg">
+        <div className="box right">
+          <div className="content">
+            <h2> The Ceremony</h2>
+            <p>The Ceremony will take place at the Sun Pavilion, which is within easy walking distance of the centre of Harrogate. The ceremony will begin promptly at 1:30 pm. If you are coming from further afield please give yourself plenty of time to get there as Harrogate traffic can be heavy on a Saturday. The grooms party will be enjoying a drink in the Fat Badger Public House beforehand if you wish to arrive earlier. After the ceremony, transport will be provided to the reception venue, where there is limited space for parking.</p>
+            <div className="links">
+              <Link href="http://thefatbadgerharrogate.com" rel="noopener noreferrer" target="_blank">The Fat Badger</Link>
+              <Link href="http://www.sunpavilion.co.uk/" rel="noopener noreferrer" target="_blank" className="Pavillion">The Sun Pavillion</Link>
             </div>
           </div>
         </div>
+      </div>
     </div>
 
-<div className="row">
-<div className="col-lg-7 col-xs-12 first-lg">
-  <div className="box left">
-    <div className="content">
-      <h2>Dress Code</h2>
-      <p>            
-      Semi-formal attire. Jacket & a tie is sufficient for men, summer
-      dresses for ladies. Given the reception will be held in the garden,
-      we would suggest that ladies, in particular, wear appropriate
-      footwear. We would also advise that you avoid wearing yellow- it
-      appears to be particularly attractive to the garden's fauna.
-      </p>
+    <div className="row">
+      <div className="col-lg-7 col-xs-12 first-lg">
+        <div className="box left">
+          <div className="content">
+            <h2>Dress Code</h2>
+            <p>Semi-formal attire. Jacket & a tie is sufficient for men, summer dresses for ladies. Given the reception will be held in the garden, we would suggest that ladies, in particular, wear appropriate footwear. We would also advise that you avoid wearing yellow- it appears to be particularly attractive to the garden's fauna.</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-5 col-xs-12 first-xs">
+        <div className="box">
+          <div className="image image2" alt="Socks" />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-<div className="col-lg-5 col-xs-12 first-xs">
-  <div className="box">
-    <div className="image image2" alt="Sun Pavillion Image" />
-  </div>
-</div>
-</div>
 
-<div className="row">
-<div className="col-lg-5 col-xs-12 first-xs">
-  <div className="box">
-    <div className="image image3" alt="Sun Pavillion Image" />
-  </div>
-</div>
-<div className="col-lg-7 col-xs-12 first-lg">
-  <div className="box right">
-    <div className="content">
-      <h2>Getting Home</h2>
-        <p>
-          Harrogate taxis get very busy on a Saturday night, so it is
-          advisable to prebook your ride home for the end of the night. The
-          wedding will be well signposted, however, taxis can be advised that
-          Hay a Park is half a mile past Knaresborough Rugby Club.
-        </p>
-        <p>
+    <div className="row">
+      <div className="col-lg-5 col-xs-12 first-xs">
+        <div className="box">
+          <div className="image image3" alt="Socks" />
+        </div>
+      </div>
+      <div className="col-lg-7 col-xs-12 first-lg">
+        <div className="box right">
+          <div className="content">
+            <h2>Getting Home</h2>
+            <p>Harrogate taxis get very busy on a Saturday night, so it is advisable to prebook your ride home for the end of the night. The wedding will be well signposted, however, taxis can be advised that Hay a Park is half a mile past Knaresborough Rugby Club.</p>
+            <p>
           <span>Address : </span>
           <Link
             href="https://goo.gl/maps/kKdfeJATntC93xSa6"
@@ -91,7 +76,8 @@ const WeddingPage = () => (
           >
             The Stables, Hall Farm Barns, Hay a Park, Knaresborough, HG5 0SN
           </Link>
-          <br />
+          </p>
+          <p>
           <Link
             href="http://bluelinetaxis.co.uk/"
             rel="noopener noreferrer"
@@ -100,91 +86,92 @@ const WeddingPage = () => (
             Blue Line Taxis
           </Link>
         </p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
   </Wedding>
-  </div>
 );
-
-const Banner = styled.div`
-  .imageBanner {
-    background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787910/savethedate_zk4gwg.jpg");
-   background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100%;
-    height: 100vh;
-  }
-`;
-
+      
+      
 const Wedding = styled.div`
 .top {
-  padding-top: 5em;
+padding-top: 5em;
+}
+.title {
+  margin-bottom: 1em;
+  padding-top: 3em;
 }
 .content {
-    padding: 5rem;
-  }
+padding: 5rem;
+}
 
 h2 {
-    font-size: 2.25rem;
-    font-weight: 400;
-    letter-spacing: -0.05em;
-    margin: 0 0 1rem 0;
-  }
+font-size: 2.25rem;
+font-weight: 400;
+letter-spacing: -0.05em;
+margin: 0 0 1rem 0;
+}
 
-  p, a {
-    font-size: 1.25rem;
-    font-weight: 300;
-    line-height: 1.7;
-  }
+p, a {
+font-size: 1.25rem;
+font-weight: 300;
+line-height: 1.7;
+}
 
-  .Pavillion {
-    margin-left: 4em;
-  }
+.Pavillion {
+margin-left: 4em;
+}
 
 .left {
-  text-align: right;
+text-align: right;
 }
 .right {
-  text-align: left;
+text-align: left;
 }
-  .image {
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100%;
-    height: 55vh;
-  }
-    }
-  .image1 {
-    background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787920/paintedSunPav_qzolvv.jpg");
-  }
-  .image2 {
-    background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787849/shades.jpg");
-  }
-  .image3 {
-    background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787854/kayak_ww3qgr.jpg");
-  }
+.center{
+  text-align: center;
+}
+.image {
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+width: 100%;
+height: 55vh;
+}
+.first {
+  height: 100vh;
+}
+.image1 {
+background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787920/paintedSunPav_qzolvv.jpg");
+}
+.image2 {
+background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787849/shades.jpg");
+}
+.image3 {
+background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787854/kayak_ww3qgr.jpg");
+}
+.image4 {
+background-image: url("https://res.cloudinary.com/eddemott/image/upload/f_auto,q_auto/v1556787910/savethedate_zk4gwg.jpg");
+}
 
-  @media only screen and (max-width: 1180px) {
-    .content {
-      padding: 2em;
-    }
-    h2, p, a {
-      text-align: center;
-    }
-    .top {
-      padding-top: 0;
-    }
-    .image {
-    height: 30vh;
-    }
-    .Pavillion {
-    margin-left: 1em;
-  
-  }
+@media only screen and (max-width: 1180px) {
+.content {
+padding: 2em;
+}
+h2, p, a {
+text-align: center;
+}
+.top {
+padding-top: 0;
+}
+.image {
+height: 30vh;
+}
+.Pavillion {
+margin-left: 1em;
+}
+}
 `;
 
 export default WeddingPage;
